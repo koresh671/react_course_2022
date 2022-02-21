b418e2fc2e25c68990c27c99dcf10beb0409b784
koresh671 <sergei.menskii@mail.ru>
2022-02-21 21:00:50 +0300

    lesson_2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import ProductCounter from "./ProductCounter";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <ProductCounter product={{ productName: "Orange" }} />
+      <ProductCounter product={{ productName: "Apple", count: 10 }} />
+      <ProductCounter product={{ productName: "Pineapple", count: 35 }} />
+      <ProductCounter product={{ productName: "Watermelon", count: 0 }} />
     </div>
   );
 };
